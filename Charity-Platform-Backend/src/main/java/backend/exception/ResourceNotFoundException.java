07b6e91714e3b4910d72972fcd0e22fc8d59287e
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-11-29 19:41:50 +0200

    add exceptions

---
+++ Charity-Platform-Backend/src/main/java/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
