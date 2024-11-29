554a9eb86e9335a63a226368adb24ccba4848a1c
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-11-29 21:50:27 +0200

    update header

--- Charity-Platform-Frontend/src/app/hero.tsx
+++ Charity-Platform-Frontend/src/app/hero.tsx
@@ -7,30 +7,42 @@ import { Input, Typography, Button } from "@material-tailwind/react";
 
 function Hero() {
   return (
-    <header className="mt-5 bg-white p-8">
-    <div className="w-w-full container mx-auto pt-6 pb-12 text-center">
-      <Typography
-        color="blue-gray"
-        className="mx-auto w-full text-[30px] lg:text-[48px] font-bold leading-[45px] lg:leading-[60px] lg:max-w-2xl"
-      >
-        Благодійна платформа "OPEN HEARTS"
-      </Typography>
-      <Typography
-        variant="lead"
-        className="mx-auto mt-8 mb-4 w-full px-8 !text-gray-700 lg:w-10/12 lg:px-12 xl:w-8/12 xl:px-20"
-      >
-        Від Української Біржі Благодійності
-      </Typography>
-    </div>
-    <div className="lg:container lg:mx-auto flex justify-center items-center">
-  <Image
-    width={800}
-    height={800}
-    src="/image/logos/open-hearts.png"
-    alt="credit cards"
-    className="h-96 rounded-lg lg:h-[21rem]"
-  />
+    <header className=" bg-white">
+<div className="w-full container mx-auto mb-0 flex flex-col lg:flex-row items-center lg:justify-between text-center lg:text-left">
+  {/* Image Section */}
+  <div className="lg:w-1/2 flex justify-center lg:justify-start ml-10 mb-0">
+    <Image
+      width={600}
+      height={600} // Set both width and height to the same value for a square image
+      src="/image/logos/logo.jpg"
+      alt="credit cards"
+      className="rounded-lg object-cover" // Ensures the image covers the square container and looks clean
+    />
+  </div>
+  
+  {/* Text Section */}
+  <div className="lg:w-1/2">
+    <Typography
+      color="blue-gray"
+      className="flex justify-center w-full text-[24px] lg:text-[36px] font-extrabold leading-[30px] lg:leading-[40px] lg:max-w-2xl text-gradient bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-indigo-600 to-purple-700"
+    >
+      Благодійна платформа
+    </Typography>
+    <Typography
+      color="blue-gray"
+      className="flex justify-center w-full text-[24px] lg:text-[38px] mt-6 font-extrabold leading-[30px] lg:leading-[40px] lg:max-w-2xl text-gradient bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-indigo-600 to-purple-700"
+    >
+     "OPEN HEARTS"
+    </Typography>
+    <Typography
+      variant="paragraph"
+      className=" flex justify-center mt-6 mb-4 w-full px-8 !text-gray-700 lg:w-full xl:px-0 text-sm lg:text-base font-medium text-gray-600"
+    >
+      Від Української Біржі Благодійності
+    </Typography>
+  </div>
 </div>
+
 
   </header>
   );
